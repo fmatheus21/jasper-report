e3819a9d16b6f7d331f97b92482250a83988c759
Fernando Matheus <fernando.matheuss@hotmail.com>
2022-10-10 02:34:20 -0300

    Remoção de método.

--- src/main/java/com/fmatheus/app/controller/resource/ClientReportResource.java
+++ src/main/java/com/fmatheus/app/controller/resource/ClientReportResource.java
@@ -2,8 +2,6 @@ package com.fmatheus.app.controller.resource;
 
 import com.fmatheus.app.controller.constant.HttpStatusConstant;
 import com.fmatheus.app.controller.constant.OperationConstant;
-import com.fmatheus.app.controller.constant.ResourceConstant;
-import com.fmatheus.app.controller.dto.response.ClienteDtoResponse;
 import com.fmatheus.app.controller.enumerable.ReportTypeEnum;
 import com.fmatheus.app.controller.rule.ClienteReportRule;
 import io.swagger.v3.oas.annotations.Operation;
@@ -12,14 +10,13 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Collection;
-import java.util.Objects;
 
 
 @RestController
@@ -43,18 +40,5 @@ public class ClientReportResource {
         this.rule.findAll(response, type);
     }
 
-    /*@Operation(summary = OperationConstant.GET, description = OperationConstant.CLIENT_GET_DESCRIPTION,
-            tags = {OperationConstant.CLIENT_TAG})
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = HttpStatusConstant.OK_NUMBER, description = HttpStatusConstant.OK,
-                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ClienteDtoResponse.class))),
-            @ApiResponse(responseCode = HttpStatusConstant.BAD_REQUEST_NUMBER, description = HttpStatusConstant.BAD_REQUEST_NUMBER,
-                    content = @Content(schema = @Schema(hidden = true)))
-    })*/
-    @GetMapping(ResourceConstant.ID)
-    public ResponseEntity<ClienteDtoResponse> findAll(@PathVariable int id) {
-        var result = this.rule.findById(id);
-        return Objects.nonNull(result) ? ResponseEntity.ok(result) : ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-    }
 
 }
